0893b1b4bef6f9402e023c601506d6ce5f082491
ahadkhaan97 <ahad.sartaj.ew@gmail.com>
2023-01-24 16:33:53 +0500

    submitted

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    print(solution("AABAA", "BBAAA"))
+}
+
+fun solution(s: String, t: String): Int {
+    val myMap1 = IntArray(26) { 0 }
+    val myMap2 = IntArray(26) { 0 }
+
+    for (i in s.indices) {
+        myMap1[s[i] - 'A']++
+        myMap2[t[i] - 'A']++
+    }
+
+    var count = 0
+
+    for (i in 0 until 26) {
+        if (myMap2[i] > myMap1[i])
+            count += myMap2[i] - myMap1[i]
+    }
+    return count
+}
